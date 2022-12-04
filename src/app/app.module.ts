89d04eb43d14111ce8253dd75e32dd3acb2775af
vamseeuxd <vamsee.uxd@gmail.com>
2022-12-04 21:02:30 +0530

    feat: done with login module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+import {AppRoutingModule} from './app-routing.module';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        'auth_type': 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '<your-tos-link>',
+  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUo-rJLDflDXS7mY1BHsMvfJTs2r0PvkI",
+  authDomain: "lent-it.firebaseapp.com",
+  projectId: "lent-it",
+  storageBucket: "lent-it.appspot.com",
+  messagingSenderId: "882105074732",
+  appId: "1:882105074732:web:75aca4e26356af1b883d89"
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +53,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
